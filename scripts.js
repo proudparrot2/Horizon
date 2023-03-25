befc86d42781bf68e261b289a402bb3bcce957b5
Alpha <117465385+mathhelpforkids@users.noreply.github.com>
2023-03-25 12:30:38 -0400

    test

--- scripts.js
+++ scripts.js
@@ -35,3 +35,4 @@ y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
 document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+function ShowTime = alert("m + "/" + d + "/" + y;)
